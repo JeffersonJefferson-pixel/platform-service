b913c6d9595cc722dead5488caedb90d3ce20013
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-07-24 14:07:07 +0700

    init platform service

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+  public interface IPlatformRepo
+  {
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlaformById(int id);
+    void CreatePlatform(Platform plat);
+  }
+}
